6187df99ba4daf2a219afba37e49c51ddbb2fd4e
Yankxz98 <152324053+Yankxz98@users.noreply.github.com>
2025-10-15 20:56:37 -0300

    versao com todas as musicas

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Register services
+//save
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 // Register our custom services
